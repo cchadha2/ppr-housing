71f2eac8454a00bc0b81e21a9e6b54ccc69295b8
cchadha2 <chadhac@tcd.ie>
2019-01-09 14:21:59 +0000

    Updating logging in classes and amending variable names in preprocessing

--- source/preprocessing/preprocessing.py
+++ source/preprocessing/preprocessing.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import logging
+from tqdm import tqdm
 
 from source.preprocessing.geocoding import Geocoding
 
@@ -9,10 +10,15 @@ ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
 logger.addHandler(ch)
 
+tqdm.pandas()
+
 
 class PreprocessingPPR:
 
     def __init__(self, df, api_key, geocoding=True):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+
         self.df = df
         self.df = self._remove_unnecessary_rows(self.df)
         self.df = self._time_subset(self.df)
@@ -61,30 +67,25 @@ class PreprocessingPPR:
         df = df.drop('coordinates', axis=1)
         return df
 
-    def num_preprocessing(self):
+    def num_processing(self):
+        self.logger.info('One-hot encoding data')
         # One-hot encode categorical variables.
-        self.df = pd.concat([self.df, pd.get_dummies(self.df[['Property Size Description',
-                                                              'Description of Property',
-                                                              'VAT Exclusive',
-                                                              'Not Full Market Price',
-                                                              'County',
+        self.df = pd.concat([self.df, pd.get_dummies(self.df[['Property Size Description', 'Description of Property',
+                                                              'VAT Exclusive', 'Not Full Market Price', 'County',
                                                               'Postal Code']])], axis=1)
-        self.df = self.df.drop(['County',
-                                'Date of Sale',
-                                'Property Size Description',
-                                'Description of Property',
-                                'VAT Exclusive',
-                                'Not Full Market Price',
-                                'Postal Code'], axis=1)
+        self.df = self.df.drop(['County', 'Date of Sale', 'Property Size Description', 'Description of Property',
+                                'VAT Exclusive', 'Not Full Market Price', 'Postal Code'], axis=1)
 
         if self.geocoding is True:
+            self.logger.info('Geocoding addresses')
             self.df = self._geocoding(self.df)
         else:
             self.df = self._no_geocoding(self.df)
 
         return self.df
 
-    def cat_preprocessing(self):
+    def cat_processing(self):
+        self.logger.info('Processing data for CatBoost')
         # Set index to 'Date of Sale'
         self.df = self.df.set_index('Date of Sale')
 
@@ -93,6 +94,7 @@ class PreprocessingPPR:
         self.df['Property Size Description'][self.df['Property Size Description'].isna()] = 'None'
 
         if self.geocoding is True:
+            self.logger.info('Geocoding addresses')
             self.df = self._geocoding(self.df)
         else:
             self.df = self._no_geocoding(self.df)
@@ -101,17 +103,17 @@ class PreprocessingPPR:
 
 
 def main():
-    logger.info('Preprocessing data')
+    logger.info('Pre-processing data')
     try:
         df = pd.read_csv('data/PPR-ALL.zip', encoding='latin-1', compression='zip')
     except OSError as e:
         logger.error(e)
         raise
 
-    preprocess = PreprocessingPPR(df, 'api-key')
-    processed_df = preprocess.cat_preprocessing()
+    preprocessed_df_object = PreprocessingPPR(df, 'api-key')
+    processed_df = preprocessed_df_object.cat_processing()
 
-    logger.info('Saving dataframe to CSV')
+    logger.info('Saving DataFrame to CSV')
     try:
         processed_df.to_csv('output/processed_ppr.csv', index=False)
     except OSError as e:
