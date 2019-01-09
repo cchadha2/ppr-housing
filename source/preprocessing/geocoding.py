f5fe875abc4d691526583454f1cdd1699d8abd04
cchadha2 <chadhac@tcd.ie>
2019-01-09 22:00:29 +0000

    Moved logging in geocoding class into __init__

--- source/preprocessing/geocoding.py
+++ source/preprocessing/geocoding.py
@@ -1,21 +1,18 @@
 import numpy as np
 import requests
 import logging
-from tqdm import tqdm
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-logger.addHandler(ch)
-
-tqdm.pandas()
 
 
 class Geocoding:
     geo_url = 'https://maps.googleapis.com/maps/api/geocode/json?address='
 
     def __init__(self, address, api_key):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        self.logger.addHandler(ch)
+
         add_list = address.split()
         add_list = [line + '+' for line in add_list[:-1]]
         add_list.append(address.split()[-1])
@@ -23,17 +20,17 @@ class Geocoding:
         self.url = self.geo_url + add_url + '&key=' + api_key
 
     def lat_lng(self):
-        logger.debug('Requesting geospatial info from Google API')
+        self.logger.debug('Requesting geospatial info from Google API')
         try:
             response = requests.get(self.url)
             response.raise_for_status()
         except requests.exceptions.HTTPError as e:
-            logger.error(e)
+            self.logger.error(e)
 
         results = response.json()['results']
         if len(results) != 0:
-            logger.debug('Found geospatial coordinates')
+            self.logger.debug('Found geospatial coordinates')
             return results[0]['geometry']['location']['lat'], results[0]['geometry']['location']['lng']
         else:
-            logger.debug('Could not find geospatial coordinates')
+            self.logger.debug('Could not find geospatial coordinates')
             return np.nan, np.nan
